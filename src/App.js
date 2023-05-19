f7b9ffd8ac8e28901909a4863c631a82e9b55a0b
TalaviyaHemali <hemalitalaviya77@gmail.com>
2023-05-19 17:40:38 +0530

    Date 19

--- src/App.js
+++ src/App.js
@@ -12,11 +12,15 @@ import ReceiveParcel from './Components/Branch/ReceiveParcel/ReceiveParcel';
 import PendingParcel from './Components/Branch/PendingParcel/PendingParcel';
 import Addbranchparcel from './Components/Branch/Addbranchparcel/Addbranchparcel';
 import BranchReport from './Components/Branch/BranchReport/BranchReport';
-
+import Dashboard from "./Components/Admin/Dashboard/Dashboard";
+import Addbranch from "./Components/Admin/Addbranch/Addbranch";
+import ReportDetail from "./Components/Admin/ReportDetail/ReportDetail";
+import EditBranch from "./Components/EditBranch/EditBranch";
 
 function App() {
   return (
     <>
+
       <Routes>
         <Route path="/" element={<Branchdash />} />
         <Route path="/branchinfo" element={<BranchInfo />} />
@@ -26,6 +30,13 @@ function App() {
         <Route path="/pendingparcel" element={<PendingParcel />} />
         <Route path="/addbranchparcel" element={<Addbranchparcel />} />
         <Route path="branchreport" element={<BranchReport />} />
+
+
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addbranch" element={<Addbranch />} />
+        <Route path="/branchtable" element={<EditBranch />} />
+        <Route path="/reportdetails" element={<ReportDetail />} />
+
       </Routes>
 
     </>
