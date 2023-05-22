87fa7820541659e0f9c91b70a7dae3cd554ed614
hemanshi ramani <hemanshiramani2003@gmail.com>
2023-05-22 17:51:14 +0530

    22 May

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,} from 'react-router-dom';
 import Branchdash from './Components/Branch/Branchdash/Branchdash';
 import BranchInfo from './Components/Branch/BranchInfo/BranchInfo';
 import Branchstaff from './Components/Branch/Branchstaff/Branchstaff';
@@ -12,15 +8,13 @@ import ReceiveParcel from './Components/Branch/ReceiveParcel/ReceiveParcel';
 import PendingParcel from './Components/Branch/PendingParcel/PendingParcel';
 import Addbranchparcel from './Components/Branch/Addbranchparcel/Addbranchparcel';
 import BranchReport from './Components/Branch/BranchReport/BranchReport';
-
 import Landingpage from "./Components/User/Landingpage/Landingpage";
 import Usertrackparcel from "./Components/User/Usertrackparcel/Usertrackparcel"
 import Login from './Components/Login/Login';
 import Dashboard from './Components/Admin/Dashboard/Dashboard';
 import Addbranch from "./Components/Admin/Addbranch/Addbranch";
 import EditBranch from './Components/EditBranch/EditBranch';
-import ReportDetail from "./Components/Admin/ReportDetail/ReportDetail"
-
+import ReportDetail from "./Components/Admin/ReportDetail/ReportDetail";
 
 
 function App() {
